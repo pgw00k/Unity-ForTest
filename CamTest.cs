a8fd289c86522ba126abdad0e09d7ba9854fefcb
pgw00k <pgw00k@gmail.com>
2018-01-17 16:16:17 +0800

    Update CamTest.cs

--- CamTest.cs
+++ CamTest.cs
@@ -1,31 +1,53 @@
-﻿using UnityEngine;  
-using System.Collections;  
+//2018-1-17
+//镜头跟随鼠标变化
+//v1-基本操作
 
-public class CamTest : MonoBehaviour  
-{  
-	private float speed = 0;//
+using UnityEngine;
+using System.Collections;
+
+public class CamTest : MonoBehaviour
+{
+    private float SpeedTransform = 1f;//镜头位移速度
     private float SpeedRota = 10.0f;//镜头旋转速度
+    private float SpeedZoom = 5.0f;//镜头缩放速度
 
     private Vector3 V3_Rota;
-	
-	// Use this for initialization  
-	void Start()  
-	{  
-	}  
-	
-	// Update is called once per frame  
-	void Update()  
-	{
-        if (Input.GetMouseButton(2))
+
+    //变换开关
+    private bool bTransform;
+    private bool bRotate;
+
+    // Use this for initialization  
+    void Start()
+    {
+        bTransform = false;
+        bRotate = false;
+    }
+
+    // Update is called once per frame  
+    void Update()
+    {
+        bTransform = Input.GetMouseButton(2);
+        bRotate = Input.GetMouseButton(1);
+        if (bRotate)
         {
-            FollowMouse();
+            RotateByMouse();
         }
-	}
+        if (bTransform)
+        {
+            TranslateByMouse();
+        }
+        if (Input.GetAxis("Mouse ScrollWheel") !=0)
+        {
+            Debug.LogFormat("CameTest:Mouse ScrollWheel:{0}",Input.GetAxis("Mouse ScrollWheel"));
+            ScaleByMouse();
+        }
+    }
 
     /// <summary>
     /// 镜头跟随鼠标旋转
     /// </summary>
-    void FollowMouse()
+    void RotateByMouse()
     {
         float xRot = Input.GetAxis("Mouse Y") * (-1) * SpeedRota;
         float yRot = Input.GetAxis("Mouse X") * SpeedRota;
@@ -34,10 +56,30 @@ public class CamTest : MonoBehaviour
         RotateOnXY(V3_Rota);
     }
 
-    bool RotateOnXY(Vector3 v3)
+    void RotateOnXY(Vector3 v3)
     {
         this.transform.RotateAround(this.transform.position, Vector3.up, v3.y);
         this.transform.RotateAround(this.transform.position, this.transform.right, v3.x);
-        return true;
     }
-}  
+
+    /// <summary>
+    /// 镜头跟随鼠标位移
+    /// </summary>
+    void TranslateByMouse()
+    {
+        float y = Input.GetAxis("Mouse Y") * SpeedTransform;
+        float x = Input.GetAxis("Mouse X") * SpeedTransform;
+
+        transform.Translate(x,y,0,Space.Self);
+
+        Debug.LogFormat("CamTest:X = {0},Y = {1}",x,y);
+    }
+
+
+
+    void ScaleByMouse()
+    {
+        float z = Input.GetAxis("Mouse ScrollWheel") * SpeedZoom;
+        transform.Translate(0, 0, z,Space.Self);
+    }
+}
